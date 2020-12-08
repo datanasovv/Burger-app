4b7828bf752a4e3cf2dc62640dcfe6da0ae021a5
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2020-12-08 22:59:01 +0200

    Setting up the project
    Layout component and container

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import Burger from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Layout>
+          <Burger />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
